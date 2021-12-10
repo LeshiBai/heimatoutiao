8d5df3f97aa9171261687867c6ec242f042287c7
柏乐识 <1090522609@qq.com>
2021-12-10 10:43:50 +0800

    项目初始化--引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="iconfont icon-dianzan"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-
+import vant from 'vant'
+import 'vant/lib/index.css'
 import './styles/index.less'
+
+Vue.use(vant)
 
 Vue.config.productionTip = false
 
